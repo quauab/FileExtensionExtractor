2612f2b671d9a2087bf35289c4aaa2abfc9c70b2
quauab <ichglauben@gmail.com>
2016-05-09 10:31:44 -0400

    Added comments to the AbstractExtensionExtractor and ExtensionExtractor classes

--- fileextensionextractor/src/main/java/com/gmail/ichglauben/fileextensionextractor/core/abstracts/AbstractExtensionExtractor.java
+++ fileextensionextractor/src/main/java/com/gmail/ichglauben/fileextensionextractor/core/abstracts/AbstractExtensionExtractor.java
@@ -5,15 +5,27 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import com.gmail.ichglauben.fileextensionextractor.utils.abstracts.CustomClass;
-
+/**	
+ * Abstract class used to extract a file's extension. This class provides 3 overloaded static methods to accomplish this task.
+ *	@see java.io.File
+ *	@see java.nio.file.Path
+ *	@see java.nio.file.Paths
+ *	@author	Rick Walker
+ *	@version 0.1
+ *	@since 5/8/2016
+*/
 public abstract class AbstractExtensionExtractor extends CustomClass {
+	/**Single default constructor*/
 	public AbstractExtensionExtractor() {
 		super();
 	}
 
-	protected static String extractExtension(String file_path) {
-		if (null != file_path && (!file_path.isEmpty()) && (file_path.length() > 0)) {
-			Path path = Paths.get(file_path);
+	/**Use this method to extract the extension from the file at the end of this file path.
+	 * @param filePath - <b><i>String</i></b> The file at the end of this path
+	 * @return <b><i>String</i></b> The file's extension, including the dot*/
+	protected static String extractExtension(String filePath) {
+		if (null != filePath && (!filePath.isEmpty()) && (filePath.length() > 0)) {
+			Path path = Paths.get(filePath);
 			if (path.toFile().exists() && path.toFile().isFile()) {
 				String name = path.getFileName().toString();
 				int index = name.lastIndexOf(".");
@@ -25,6 +37,9 @@ public abstract class AbstractExtensionExtractor extends CustomClass {
 		return null;
 	}
 
+	/**Use this method to extract the extension from this Path.
+	 * @param path - <b><i>Path</i></b> The absolute Path
+	 * @return <b><i>String</i></b> The file's extension, including the dot*/
 	protected static String extractExtension(Path path) {
 		if (null != path) {
 			if (path.toFile().exists() && path.toFile().isFile()) {
@@ -38,6 +53,9 @@ public abstract class AbstractExtensionExtractor extends CustomClass {
 		return null;
 	}
 
+	/**Use this method to extract the extension from this File.
+	 * @param file - <b><i>File</i></b> The absolute File
+	 * @return <b><i>String</i></b> The file's extension, including the dot*/
 	protected static String extractExtension(File file) {
 		if (null != file) {
 			if (file.exists() && file.isFile()) {
--- fileextensionextractor/src/main/java/com/gmail/ichglauben/fileextensionextractor/core/concretes/ExtensionExtractor.java
+++ fileextensionextractor/src/main/java/com/gmail/ichglauben/fileextensionextractor/core/concretes/ExtensionExtractor.java
@@ -4,12 +4,24 @@ import java.io.File;
 import java.nio.file.Path;
 
 import com.gmail.ichglauben.fileextensionextractor.core.abstracts.AbstractExtensionExtractor;
-
+/**	
+ * Concrete class that extends the AbstractExtensionExtractor class. Use this class to extract the extension from a file. This class provides
+ * 6 overloaded methods to accomplish this task.
+ *	@see java.io.File
+ *	@see java.nio.file.Path
+ *	@author	Rick Walker
+ *	@version 0.1
+ *	@since 5/8/2016
+*/
 public class ExtensionExtractor extends AbstractExtensionExtractor {
+	/**Single default constructor that overrides it's parent*/
 	public ExtensionExtractor() {
 		super();
 	}
 
+	/**Use this method to extract the extension of the file referenced by this Path.
+	 * @param path - <b><i>Path</i></b> The absolute Path
+	 * @return <b><i>String</i></b> The file's extension, including the dot*/
 	public static String extract(Path path) {
 		if (null != path) {
 			return extractExtension(path);
@@ -17,6 +29,9 @@ public class ExtensionExtractor extends AbstractExtensionExtractor {
 		return null;
 	}
 
+	/**Use this method to extract the extension of this File.
+	 * @param file - <b><i>File</i></b> The absolute File
+	 * @return <b><i>String</i></b> The file's extension, including the dot*/
 	public static String extract(File path) {
 		if (null != path) {
 			return extractExtension(path);
@@ -24,13 +39,20 @@ public class ExtensionExtractor extends AbstractExtensionExtractor {
 		return null;
 	}
 
-	public static String extract(String path) {
-		if (null != path) {
-			return extractExtension(path);
+	/**Use this method to extract the extension from the file at the end of this file-path.
+	 * @param filePath - <b><i>String</i></b> The absolute file path
+	 * @return <b><i>String</i></b> The file's extension, including the dot*/
+	public static String extract(String filePath) {
+		if (null != filePath) {
+			return extractExtension(filePath);
 		}
 		return null;
 	}
 
+	/**Use this method to extract the extension of the file referenced by this Path.
+	 * @param path - <b><i>Path</i></b> The absolute Path
+	 * @param withoutDot <b><i>boolean</i></b> If true, return the extension without the dot
+	 * @return <b><i>String</i></b> The file's extension, excluding the dot*/
 	public static String extract(Path path, boolean withoutDot) {
 		if (null != path) {
 			if (withoutDot) {
@@ -48,35 +70,43 @@ public class ExtensionExtractor extends AbstractExtensionExtractor {
 		return null;
 	}
 
-	public static String extract(File path, boolean withoutDot) {
-		if (null != path) {
+	/**Use this method to extract the extension of this File.
+	 * @param file - <b><i>File</i></b> The absolute File
+	 * @param withoutDot - <b><i>boolean</i></b> If true, return the extension without the dot
+	 * @return <b><i>String</i></b> The file's extension, excluding the dot*/
+	public static String extract(File file, boolean withoutDot) {
+		if (null != file) {
 			if (withoutDot) {
 				String ext = null;
-				if (null != (ext = extractExtension(path))) {
+				if (null != (ext = extractExtension(file))) {
 					int dot = ext.lastIndexOf(".");
 					if (dot != -1) {
 						return ext.substring(dot + 1);
 					}
 				}
 			} else {
-				return extractExtension(path);
+				return extractExtension(file);
 			}
 		}
 		return null;
 	}
 
-	public static String extract(String path, boolean withoutDot) {
-		if (null != path) {
+	/**Use this method to extract the extension from the file at the end of this file-path.
+	 * @param filePath - <b><i>String</i></b> The absolute file path
+	 * @param withoutDot - <b><i>boolean</i></b> If true, return the extension without the dot
+	 * @return <b><i>String</i></b> The file's extension, excluding the dot*/
+	public static String extract(String filePath, boolean withoutDot) {
+		if (null != filePath) {
 			if (withoutDot) {
 				String ext = null;
-				if (null != (ext = extractExtension(path))) {
+				if (null != (ext = extractExtension(filePath))) {
 					int dot = ext.lastIndexOf(".");
 					if (dot != -1) {
 						return ext.substring(dot + 1);
 					}
 				}
 			} else {
-				return extractExtension(path);
+				return extractExtension(filePath);
 			}
 		}
 		return null;
